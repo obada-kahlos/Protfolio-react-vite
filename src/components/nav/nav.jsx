b1729273f85796969b9c3f266292d4ca6c55f755
obada-kahlos <obadakahlous8@gmail.com>
2023-07-05 19:23:32 +0300

    equivalent code in normal CSS in toggle button

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -74,33 +74,30 @@ const Nav = () => {
             background-color: #7B7B7B;
             border-radius: 9999px;
             box-shadow: 10px 0px 0 0 rgba(#000000, 0.2) inset;
-        
-            &::before,
-            &::after {
-                position: absolute;
-                content: '';
-                display: block;
-                background-color: #FFFFFF;
-                border-radius: 9999px;
-            }
-        
-            &::before {
-                top: 5px;
-                left: 5px;
-                width: 6px;
-                height: 6px;
-                background-color: #FFFFFF;
-                opacity: 0.6;
-            }
-        
-            &::after {
-                bottom: 7px;
-                right: 7px;
-                width: 8px;
-                height: 8px;
-                background-color: #FFFFFF;
-                opacity: 0.8;
-            }
+        }
+        .switch__indicator::before,.switch__indicator::after {
+            position: absolute;
+            content: '';
+            display: block;
+            background-color: #FFFFFF;
+            border-radius: 9999px;
+          
+        }
+        .switch__indicator::before{
+            top: 5px;
+            left: 5px;
+            width: 6px;
+            height: 6px;
+            background-color: #FFFFFF;
+            opacity: 0.6;
+        }
+         .switch__indicator::after{
+            bottom: 7px;
+            right: 7px;
+            width: 8px;
+            height: 8px;
+            background-color: #FFFFFF;
+            opacity: 0.8;
         }
         
         .switch__decoration {
@@ -113,96 +110,90 @@ const Nav = () => {
             background-color: #FFFFFF;
             border-radius: 9999px;
             animation: twinkle 0.8s infinite -0.6s;
-        
-            &::before,
-            &::after {
-                position: absolute;
-                display: block;
-                content: '';
-                width: 5px;
-                height: 5px;
-                background-color: #FFFFFF;
-                border-radius: 9999px;
-            }
-        
-            &::before {
-                top: -20px;
-                left: 10px;
-                opacity: 1;
-                animation: twinkle 0.6s infinite;
-            }
-        
-            &::after {
-                top: -7px;
-                left: 30px;
-                animation: twinkle 0.6s infinite -0.2s;
-            }
+        }
+        .switch__decoration::after, .switch__decoration::before{
+            position: absolute;
+            display: block;
+            content: '';
+            width: 5px;
+            height: 5px;
+            background-color: #FFFFFF;
+            border-radius: 9999px;
+        }
+        .switch__decoration::before {
+            top: -20px;
+            left: 10px;
+            opacity: 1;
+            animation: twinkle 0.6s infinite;
+        }
+    
+        .switch__decoration::after {
+            top: -7px;
+            left: 30px;
+            animation: twinkle 0.6s infinite -0.2s;
         }
         
         @keyframes twinkle {
             50% { opacity: 0.2; }
         }
-        
-        .switch__indicator {
-            &,
-            &::before,
-            &::after {
-                transition: all 0.4s cubic-bezier(.46,.03,.52,.96);
-            }
+
+        .switch__indicator,
+        .switch__indicator::before,
+        .switch__indicator::after {
+            transition: all 0.4s cubic-bezier(.46,.03,.52,.96);
         }
-        
         .switch__input:checked + .switch__label {
             background-color: #8FB5F5;
             border-color: #347CF8;
-        
-            .switch__indicator {
-                background-color: #ECD21F;
-                box-shadow: none;
-                transform: translate(-50%, -50%) translateX(72%);
-        
-                &::before,
-                &::after {
-                    display: none;
-                }
-            }
-        
-            .switch__decoration {
-                top: 50%;
-                transform: translate(0%, -50%);
-                animation: cloud 8s linear infinite;
-        
-                width: 20px;
-                height: 20px;
-        
-                &::before {
-                    width: 10px;
-                    height: 10px;
-                    top: auto;
-                    bottom: 0;
-                    left: -8px;
-                    animation: none;
-                }
-        
-                &::after {
-                    width: 15px;
-                    height: 15px;
-                    top: auto;
-                    bottom: 0;
-                    left: 16px;
-                    animation: none;
-                }
-        
-                &,
-                &::before,
-                &::after {
-                    border-radius: 9999px 9999px 0 0;
-                }
-        
-                &::after {
-                    border-bottom-right-radius: 9999px;
-                }
-            }
         }
+
+        .switch__input:checked + .switch__label .switch__indicator {
+            background-color: #ECD21F;
+            box-shadow: none;
+            transform: translate(-50%, -50%) translateX(72%);
+        }
+
+        .switch__input:checked + .switch__label .switch__indicator::before,
+        .switch__input:checked + .switch__label .switch__indicator::after {
+            display: none;
+        }
+
+        .switch__input:checked + .switch__label .switch__decoration {
+            top: 50%;
+            transform: translate(0%, -50%);
+            animation: cloud 8s linear infinite;
+            width: 20px;
+            height: 20px;
+        }
+
+        .switch__input:checked + .switch__label .switch__decoration::before {
+            width: 10px;
+            height: 10px;
+            top: auto;
+            bottom: 0;
+            left: -8px;
+            animation: none;
+        }
+
+        .switch__input:checked + .switch__label .switch__decoration::after {
+            width: 15px;
+            height: 15px;
+            top: auto;
+            bottom: 0;
+            left: 16px;
+            animation: none;
+        }
+
+        .switch__input:checked + .switch__label .switch__decoration,
+        .switch__input:checked + .switch__label .switch__decoration::before,
+        .switch__input:checked + .switch__label .switch__decoration::after {
+            border-radius: 9999px 9999px 0 0;
+        }
+
+        .switch__input:checked + .switch__label .switch__decoration::after {
+            border-bottom-right-radius: 9999px;
+        }
+
         
         @keyframes cloud {
             0% {
